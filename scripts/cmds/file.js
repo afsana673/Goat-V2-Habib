6f605d1aa964192d5e05b61e159d5993fa478f71
afsana673 <afsanakhatun369369@gmail.com>
2026-08-22 16:40:33 +0530

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100079043707149", "100079043707149",];
+		const permission = ["100079043707149", "100093182723125",];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" khankir chele ja vaggg bot ki tor baper 🥵💦", event.threadID, event.messageID);
 		}
